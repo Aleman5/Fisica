3515ec2fcce100177b71f93f4d3b02430ac391ff
Alejandro-san <ale.bros@hotmail.com>
2019-04-10 21:13:49 -0300

    Entregable 1 (WIP)?

--- Entregable-1-Abecasis/Assets/Scripts/BulletMovement.cs
+++ Entregable-1-Abecasis/Assets/Scripts/BulletMovement.cs
@@ -10,33 +10,43 @@ public class BulletMovement : MonoBehaviour
 
     float time;
 
+
+    Vector2 pos;
+
     const float gravity = 9.81f;
 
     private void Awake()
     {
         time = 0.0f;
+        pos = new Vector2();
     }
 
     void Update()
     {
         time += Time.deltaTime;
 
-        float speedY = (adaptedVelocity.y + (-gravity * Mathf.Sqrt(time)) / 2) * Time.deltaTime;
+        //float speedY = (adaptedVelocity.y * Time.deltaTime - gravity * Mathf.Sqrt(Time.deltaTime) * 0.5f) * Time.deltaTime;
+        pos.x += adaptedVelocity.x * Time.deltaTime;
+        pos.y += (adaptedVelocity.y * Time.deltaTime - gravity * Mathf.Sqrt(Time.deltaTime) * 0.5f);
 
-        transform.Translate(
+        Vector3 v = new Vector3(pos.x, pos.y, 0.0f);
+
+        transform.position = v;
+
+        /*transform.Translate(
             adaptedVelocity.x * Time.deltaTime, // MRU
             speedY, // Tiro Vertical + Caida Libre
-            0.0f);
+            0.0f);*/
 
-        Debug.Log(speedY);
+        //Debug.Log(speedY);
     }
 
     public void CalculateVelocity(float degrees)
     {
-        // Calculate based on the angle ofthe z axis
-
-        adaptedVelocity.x = -velocity * Mathf.Cos(degrees);
+        adaptedVelocity.x = velocity * Mathf.Cos(degrees);
         adaptedVelocity.y = velocity * Mathf.Sin(degrees);
+
+        //GetComponentInChildren<Transform>().rotation = rotation;
 
         //Debug.Log("Velocity x: " + adaptedVelocity.x + " y: " + adaptedVelocity.y);
     }
--- Entregable-1-Abecasis/Assets/Scripts/Throw.cs
+++ Entregable-1-Abecasis/Assets/Scripts/Throw.cs
@@ -17,10 +17,15 @@ public class Throw : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Transform t = Instantiate(bullet, null).transform;
-            t.eulerAngles += transform.eulerAngles;
+            Transform tform = Instantiate(bullet, transform.position, Quaternion.identity).transform;
 
-            t.GetComponent<BulletMovement>().CalculateVelocity(transform.eulerAngles.y);
+            tform.position = transform.position;
+
+            //tform.eulerAngles += transform.eulerAngles;
+            
+            Debug.Log(parent.eulerAngles.z);
+
+            tform.GetComponent<BulletMovement>().CalculateVelocity(parent.eulerAngles.z);
         }
     }
 }
