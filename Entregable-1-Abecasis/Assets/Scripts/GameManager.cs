f04c26e4acfaf3ffd0c9de3b2bab689177d08edf
Alejandro <ale.bros@hotmail.com>
2019-04-18 00:20:22 -0300

    Entregable 1 (WIP)
    Add UI, Trail for Bullet and a Basic WIP Game Manager

--- Entregable-1-Abecasis/Assets/Scripts/GameManager.cs
+++ Entregable-1-Abecasis/Assets/Scripts/GameManager.cs
@@ -1,30 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    struct Tank
+    static GameManager instance;
+
+    [System.Serializable]
+    public struct Tank
     {
+        public string tankName;
+        public Transform tankTransform;
         public Move moveScript;
         public Throw throwScript;
+        public WeaponRotation weaRotScript;
     }
 
-    [SerializeField] List<Transform> tanks;
+    [SerializeField] List<Tank> tanks;
+    [SerializeField] float timePerTurn;
 
-    List<Tank> tankScripts;
+    int turnOf;
+    float timeLeft;
 
-    /*private void Awake()
+    void Awake()
     {
-        for (int i = 0; i < tanks.Count; i++)
+        for (int i = 1; i < tanks.Count; i++)
         {
-            tankScripts.Add(new Tank());
-            tankScripts[i].moveScript = tanks[i].transform.gameObject.GetComponent<Move>();
+            tanks[i].moveScript.enabled = false;
+            tanks[i].throwScript.enabled = false;
+            tanks[i].weaRotScript.enabled = false;
         }
 
-        foreach (Transform tank in tanks)
+        turnOf = 0; // Tank 0 always starts
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
         {
-            tankScripts.moveScript = tank.transform.gameObject.GetComponent<Move>();
+            tanks[turnOf].moveScript.enabled = false;
+            tanks[turnOf].throwScript.enabled = false;
+            tanks[turnOf].weaRotScript.enabled = false;
+
+            if (++turnOf == tanks.Count) turnOf = 0;
+
+            tanks[turnOf].moveScript.enabled = true;
+            tanks[turnOf].throwScript.enabled = true;
+            tanks[turnOf].weaRotScript.enabled = true;
+
+            timeLeft = timePerTurn;
         }
-    }*/
+    }
+
+    public float GetTimeLeft() { return timeLeft; }
+
+    public float GetTimePerTurn() { return timePerTurn; }
+
+    static public GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (!instance)
+                {
+                    GameObject go = new GameObject("Managers");
+                    instance = go.AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
 }
---
+++ Entregable-1-Abecasis/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Text timeLeftTxt;
+    [SerializeField] Color colorA;
+    [SerializeField] Color colorB;
+
+    float timePerTurn;
+
+    void Start()
+    {
+        timePerTurn = GameManager.Instance.GetTimePerTurn();
+    }
+
+    void Update()
+    {
+        float t = GameManager.Instance.GetTimeLeft();
+
+        timeLeftTxt.color = Color.Lerp(colorA, colorB, t / timePerTurn);
+        timeLeftTxt.text = "Time for next turn: " + t;
+    }
+}
