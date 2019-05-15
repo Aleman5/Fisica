7e641b7302e93b22de94f669162b07c74b53619f
Alejandro-san <ale.bros@hotmail.com>
2019-05-15 20:33:14 -0300

    Entregable 2 Trying to implement the new Movement

--- Entregable-2-Abecasis-Real/Assets/Scripts/Movement/PlayerMovement.cs
+++ Entregable-2-Abecasis-Real/Assets/Scripts/Movement/PlayerMovement.cs
@@ -6,20 +6,53 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
 
+    float radio = 1.0f;
+    float angle = 0.0f;
+
+    float forceX = 0.0f;
+    float forceY = 0.0f;
+
     void Update()
     {
         Vector3 addPosition = Vector3.zero;
 
-        // MRU
         if (Input.GetKey(KeyCode.S))
-        {
-            addPosition.x = Aleman5DLL.Physics.NextPositionMRU(speed);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            addPosition.x = Aleman5DLL.Physics.NextPositionMRU(-speed);
-        }
+            forceX += Time.deltaTime;
+        else if (Input.GetKey(KeyCode.X))
+            forceX -= Time.deltaTime;
+        else if (forceX > 0.0f)
+            forceX -= Time.deltaTime;
+        else if (forceX < 0.0f)
+            forceX += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+            forceY += Time.deltaTime;
+        else if (Input.GetKey(KeyCode.Z))
+            forceY -= Time.deltaTime;
+        else if (forceY > 0.0f)
+            forceY -= Time.deltaTime;
+        else if (forceY < 0.0f)
+            forceY += Time.deltaTime;
+
+        addPosition = MCU();
 
         transform.position += addPosition;
+
+
+        angle += speed * Time.deltaTime;
+        if (angle >= 360.0f) angle = 0.0f;
+
+        for (int i = 0; i < fires; i++)
+        {
+            Vector3 extraPos = Aleman5DLL.Physics.MCU(angle, radio * (i + 1));
+            fireGameObjects[i].position = transform.position + extraPos;
+        }
+    }
+
+    public Vector3 MCU(float angle, float radio, float forceX, float forceY)
+    {
+        Vector3 newPos = radio * Mathf.Cos(angle) * Vector3.right + radio * Mathf.Sin(angle) * Vector3.up;
+
+        return newPos;
     }
 }
