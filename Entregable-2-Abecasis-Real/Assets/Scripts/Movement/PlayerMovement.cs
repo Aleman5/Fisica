a808bd4fb783a8638dc8f8935d4724632f24381c
Alejandro <ale.bros@hotmail.com>
2019-06-04 12:51:54 -0300

    Entregable 2 movement Improved

--- Entregable-2-Abecasis-Real/Assets/Scripts/Movement/PlayerMovement.cs
+++ Entregable-2-Abecasis-Real/Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,16 +7,54 @@ public class PlayerMovement : MonoBehaviour
     public Wheel leftWheel;
     public Wheel rightWheel;
     public float speed;
+    public float acceleration;
+    public float friction;
 
     public float minY;
     public float maxY;
     public float minX;
     public float maxX;
 
-    //float forceX = 0.0f;
-    //float forceY = 0.0f;
-
     void Update()
+    {
+        float deltaT = Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.Z)){
+            if (Input.GetKey(KeyCode.A))
+                leftWheel.speed += acceleration * deltaT;
+            if (Input.GetKey(KeyCode.Z))
+                leftWheel.speed -= acceleration * deltaT;
+        }
+        else if(leftWheel.speed != 0){
+            leftWheel.speed = Mathf.Lerp(leftWheel.speed, 0, friction);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.C)){
+            if (Input.GetKey(KeyCode.D))
+                rightWheel.speed += acceleration * deltaT;
+            if (Input.GetKey(KeyCode.C))
+                rightWheel.speed -= acceleration * deltaT;
+        }
+        else if (rightWheel.speed != 0){
+            rightWheel.speed = Mathf.Lerp(rightWheel.speed, 0, friction );
+        }
+
+        if (leftWheel.TractionForce() == rightWheel.TractionForce()){
+                transform.position += Aleman5DLL.Physics.NextPositionMRU(rightWheel.TractionForce(), Vector3.up);
+        }else if(leftWheel.TractionForce() > rightWheel.TractionForce()){
+                transform.position += Aleman5DLL.Physics.NextPositionMRU(leftWheel.TractionForce() + rightWheel.TractionForce(),
+                                                                        Vector3.up + Vector3.left);
+        }
+        else if (leftWheel.TractionForce() < rightWheel.TractionForce()){
+            transform.position += Aleman5DLL.Physics.NextPositionMRU(rightWheel.TractionForce() + leftWheel.TractionForce(),
+                                                                    Vector3.up + Vector3.right);
+        }
+        if (transform.position.y > 0 && leftWheel.TractionForce() == 0 && rightWheel.TractionForce() == 0) { 
+            transform.position += Aleman5DLL.Physics.NextPositionMRU(acceleration, Vector3.down);
+        }
+
+    }
+
+    /*void Update()
     {
         float axis = Input.GetAxis("Horizontal");
 
@@ -54,44 +92,6 @@ public class PlayerMovement : MonoBehaviour
                     rightWheel.TractionForce(), Vector3.up + Vector3.left);
             }
         }
-    }
+    }*/
 }
 
-
-
-/*void Update()
-    {
-        Vector3 addPosition = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.S))
-            forceX += Time.deltaTime;
-        else if (Input.GetKey(KeyCode.X))
-            forceX -= Time.deltaTime;
-        else if (forceX > 0.0f)
-            forceX -= Time.deltaTime;
-        else if (forceX < 0.0f)
-            forceX += Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.A))
-            forceY += Time.deltaTime;
-        else if (Input.GetKey(KeyCode.Z))
-            forceY -= Time.deltaTime;
-        else if (forceY > 0.0f)
-            forceY -= Time.deltaTime;
-        else if (forceY < 0.0f)
-            forceY += Time.deltaTime;
-
-        addPosition = MCU();
-
-        transform.position += addPosition;
-
-
-        angle += speed * Time.deltaTime;
-        if (angle >= 360.0f) angle = 0.0f;
-
-        for (int i = 0; i < fires; i++)
-        {
-            Vector3 extraPos = Aleman5DLL.Physics.MCU(angle, radio * (i + 1));
-            fireGameObjects[i].position = transform.position + extraPos;
-        }
-    }*/
